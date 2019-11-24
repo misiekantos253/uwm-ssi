d1be998b9e12763d28473cd95c3b5cf1380dc41d
Michał Stankiewicz <misiekantos253@gmail.com>
2019-11-24 12:58:46 +0100

    some refactor

--- 01/C01.py
+++ 01/C01.py
@@ -6,14 +6,12 @@ class ColumnDataType(Enum):
      NUMBER = 'n'
 
 class C01:
-    typeFileLines = None
-    dataFileLines = None
-    separator = ' '
-    columnNameSymbol = 'a'
+    typeFileLines: [str] = []
+    dataFileLines: [str] = []
+    separator: str = ' '
+    attributeColumnNameSymbol: str = 'a'
 
     def __init__(self, typeFileName, dataFileName):
-        self.typeFileLines = []
-        self.dataFileLines = []
         for line in open(typeFileName, "r").readlines():
             self.typeFileLines.append(line.replace('\n', ''))
         for line in open(dataFileName, "r").readlines():
@@ -21,8 +19,8 @@ class C01:
         pass
 
     def main(self):
-        lastColumnValues = self.getValuesForColumn(self.getLastColumnIndex(self.dataFileLines))
-        uniqueValues = self.getUniqueValues(lastColumnValues)
+        lastColumnValues: list[str] = self.getValuesForColumn(self.getLastColumnIndex(self.dataFileLines))
+        uniqueValues: list[str] = self.getUniqueValues(lastColumnValues)
         print('Istniejące klasy decyzyjne: ', uniqueValues)
         print('\n')
 
@@ -31,24 +29,30 @@ class C01:
             print(value, ': ', self.getCountOfValue(lastColumnValues, value))
         print('\n')
 
-        print('Indeksy kolumn o typie NUMERYCZNYM: ', self.getColumnIndexesByType(ColumnDataType.NUMBER))
-        print('Indeksy kolumn o typie TEKSTOWYM: ', self.getColumnIndexesByType(ColumnDataType.STRING))
+        print('Atrybuty o typie NUMERYCZNYM: ', list(map(lambda item: str(f'{self.attributeColumnNameSymbol}{int(item) + 1}'), self.getColumnIndexesByType(ColumnDataType.NUMBER))))
+        print('Atrybuty o typie TEKSTOWYM: ', list(map(lambda item: str(f'{self.attributeColumnNameSymbol}{int(item) + 1}'), self.getColumnIndexesByType(ColumnDataType.STRING))))
         print('\n')
 
         maxDict = self.getMaxValuesForColumns(self.getColumnIndexesByType(ColumnDataType.NUMBER))
         minDict = self.getMinValuesForColumns(self.getColumnIndexesByType(ColumnDataType.NUMBER))
-        print('Maksymalne wartości dla atrybutów numerycznych:\n', maxDict)
-        print('Minimalne wartości dla atrybutów numerycznych:\n', minDict)
+        print('Maksymalne wartości dla atrybutów numerycznych:')
+        for key in maxDict.keys():
+            print(f'{key}: {maxDict[key]}')
+        print('\n')
+
+        print('Minimalne wartości dla atrybutów numerycznych:')
+        for key in minDict.keys():
+            print(f'{key}: {minDict[key]}')
         print('\n')
 
         print('Liczba różnych dostępnych wartości wszystkich atrybutów:')
-        uniqueValuesDict = self.getUniqueValuesCountForColumns(self.getAllColumnIndexes(self.dataFileLines))
+        uniqueValuesDict = self.getUniqueValuesCountForColumns(self.getAttributeColumnIndexes(self.dataFileLines))
         for key in uniqueValuesDict.keys():
             print(f'{key}: {uniqueValuesDict[key]}')
         print('\n')
 
         print('Lista różnych dostępnych wartości wszystkich atrybutów:')
-        uniqueValuesDict = self.getUniqueValuesForColumns(self.getAllColumnIndexes(self.dataFileLines))
+        uniqueValuesDict = self.getUniqueValuesForColumns(self.getAttributeColumnIndexes(self.dataFileLines))
         for key in uniqueValuesDict.keys():
             print(f'{key}: {uniqueValuesDict[key]}')
         print('\n')
@@ -59,10 +63,15 @@ class C01:
             print(f'{key}: {stdDevNumeric[key]}')
         print('\n')
 
-        print('Odchylenia standardowe dla poszczegolnych atrybutow numerycznych dla poszczególnych klas decyzyjnych:')
+        print('Odchylenia standardowe dla poszczegolnych atrybutow numerycznych dla poszczególnych klas decyzyjnych:\n')
         stdDevClasses = self.calculateStdDevForClassesForColumnIndexes(self.getColumnIndexesByType(ColumnDataType.NUMBER))
         for key in stdDevClasses.keys():
-            print(f'{key}: {stdDevClasses[key]}')
+            print(f'{key}:')
+            print('-----')
+            classDict = stdDevClasses[key]
+            for key1 in classDict.keys():
+                print(f'{key1}: {classDict[key1]}')
+            print('==============================\n')
         print('\n')
 
     def getLastColumnIndex(self, lines: list):
@@ -89,8 +98,8 @@ class C01:
                 filteredLines.append(line)
         return filteredLines
 
-    def getAllColumnIndexes(self, lines):
-        return range(len(lines[0].split(self.separator)))
+    def getAttributeColumnIndexes(self, lines):
+        return range(len(lines[0].split(self.separator)) - 1)
 
     def getUniqueValues(self, values):
         return list(set(values))
@@ -105,7 +114,7 @@ class C01:
             if (type(readType) is not str):
                 raise TypeError('Invalid data type or no data')
             if (readType == dataType.value[0]):
-                typeIndexes.append(int(line.split(self.separator)[0].split(self.columnNameSymbol)[1]) - 1)
+                typeIndexes.append(int(line.split(self.separator)[0].split(self.attributeColumnNameSymbol)[1]) - 1)
         return typeIndexes
 
     def getMinValueForColumn(self, columnIndex):
@@ -117,19 +126,19 @@ class C01:
     def getMinValuesForColumns(self, columnIndexes):
         dict: dict = {}
         for index in columnIndexes:
-            dict[f'{self.columnNameSymbol}{(index + 1)}'] = self.getMinValueForColumn(index)
+            dict[f'{self.attributeColumnNameSymbol}{(index + 1)}'] = self.getMinValueForColumn(index)
         return dict
 
     def getMaxValuesForColumns(self, columnIndexes):
         dict: dict = {}
         for index in columnIndexes:
-            dict[f'{self.columnNameSymbol}{(index + 1)}'] = self.getMaxValueForColumn(index)
+            dict[f'{self.attributeColumnNameSymbol}{(index + 1)}'] = self.getMaxValueForColumn(index)
         return dict
 
     def getUniqueValuesForColumns(self, columnIndexes: [int]):
         dict: dict = {}
         for index in columnIndexes:
-            dict[f'{self.columnNameSymbol}{(index + 1)}'] = self.getUniqueValues(self.getValuesForColumn(index))
+            dict[f'{self.attributeColumnNameSymbol}{(index + 1)}'] = self.getUniqueValues(self.getValuesForColumn(index))
         return dict
 
     def getUniqueValuesCountForColumns(self, columnIndexes: [int]):
@@ -144,13 +153,13 @@ class C01:
     def getStandardDeviationForColumnIndexes(self, columnIndexes):
         dict: dict = {}
         for index in columnIndexes:
-            dict[f'{self.columnNameSymbol}{(index + 1)}'] = self.getStandardDeviation(map(lambda item: float(item), self.getValuesForColumn(index)))
+            dict[f'{self.attributeColumnNameSymbol}{(index + 1)}'] = self.getStandardDeviation(map(lambda item: float(item), self.getValuesForColumn(index)))
         return dict
 
     def getStandardDeviationForColumnIndexesAndLines(self, columnIndexes, lines = None):
         dict: dict = {}
         for index in columnIndexes:
-            dict[f'{self.columnNameSymbol}{(index + 1)}'] = self.getStandardDeviation(map(lambda item: float(item), self.getValuesForColumnWithLines(index, lines)))
+            dict[f'{self.attributeColumnNameSymbol}{(index + 1)}'] = self.getStandardDeviation(map(lambda item: float(item), self.getValuesForColumnWithLines(index, lines)))
         return dict
 
     def calculateStdDevForClassesForColumnIndexes(self, columnIndexes):
